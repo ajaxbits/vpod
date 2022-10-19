782b3d8c1d58e4f9a1407f1f82b7fe65aa8d5749
Alex Jackson <contact@ajaxbits.com>
2022-10-19 17:07:06 +0000

    initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        
+        rust-env = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default);
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkgconfig
+            
+            rust-env
+          ];
+
+        };
+      }
+    );
+}
