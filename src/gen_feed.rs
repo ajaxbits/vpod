b18cef69a0e359cc5f401e9179d093d2ff7aa204
Alex Jackson <contact@ajaxbits.com>
2022-10-27 15:54:24 +0000

    finish implementing From<SingleVideo> for PodInfo

--- src/gen_feed.rs
+++ src/gen_feed.rs
@@ -1,4 +1,4 @@
-use chrono::{Date, NaiveDate, Utc};
+use chrono::{Date, Duration, NaiveDate, Utc};
 use rss::extension::itunes::{
     ITunesChannelExtensionBuilder, ITunesItemExtension, ITunesItemExtensionBuilder,
 };
@@ -8,6 +8,7 @@ use serde::{Deserialize, Serialize};
 use std::default;
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::str::FromStr;
 use std::time::SystemTime;
 use std::{collections::BTreeMap, env, fs::File};
 use uuid::Uuid;
@@ -42,49 +43,95 @@ fn get_recent_videos(channel_id: String) -> Vec<SingleVideo> {
         .entries
         .expect("could not extract videos from Playlist item");
 
-    println!("{videos:#?}");
+    // println!("{videos:#?}");
 
     videos
 }
 
-// #[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug)]
 struct PodInfo {
-    // #[serde(default = "default_guid")]
     guid: Guid,
-    // #[serde(default = "default_guid")]
-    // episode: u64,
+    episode: Option<i32>,
     title: String,
     duration_str: String,
     duration_secs: i64,
     author: String,
-    date: NaiveDate,
+    date: String,
     link: String,
     description: String,
 }
 
 impl From<SingleVideo> for PodInfo {
     fn from(video: SingleVideo) -> Self {
-        let duration = video.duration.unwrap().as_i64;
+        let duration = video
+            .duration
+            .expect(&format!("could not find a duration for {}", video.id))
+            .as_i64()
+            .expect("could not convert duration to i64");
         let duration = Duration::seconds(duration);
+
+        let date = NaiveDate::parse_from_str(
+            &video
+                .upload_date
+                .expect(&format!("Could not find an upload_date for {}", video.id)),
+            "%Y%m%d",
+        )
+        .expect(&format!(
+            "could not parse video {}'s upload date as str",
+            video.id,
+        ));
+
+        fn gen_description(description: Option<String>) -> String {
+            // "From the creators of the original Fallout games comes Arcanum. A fantasy RPG about the dynamics between technology and magic, and the endless despicability of gnomes.\nSupport the channel at: https://www.patreon.com/mandaloregaming or https://www.paypal.me/MandaloreGaming\nI take video suggestions at mandaloremovies@gmail.com\nTwitter: https://twitter.com/Lord_Mandalore\nBig thanks to @Branzoles for his artistic contributions.\n00:00 - Intro\n00:37 - Issues & Fixes\n01:50 - The World of Arcanum\n05:18 - Game Intro\n06:44 - Visuals\n07:53 - Music & Sound Design\n12:12 - Gameplay Mechanics\n24:42 - Questing & Story\n26:46 - Story (SPOILERS)\n48:42 - Conclusions\n50:04 - Credits\n51:28 - Baah\n\n#Arcanum #ArcanumReview #ArcanumPC #Troika",
+            if let Some(description) = description {
+                let description: String = description
+                    .split("\n")
+                    .into_iter()
+                    .map(|line| format!("<p>{line}</p>"))
+                    .collect();
+                println!("{}", description.clone());
+                description
+            } else {
+                String::new()
+            }
+        }
+
+        fn gen_duration_str(duration: Duration) -> String {
+            let hours = duration.num_hours();
+            let minutes = duration.num_minutes() - (&duration.num_hours() * 60);
+            let seconds = duration.num_seconds() - (&duration.num_minutes() * 60);
+
+            let time: Vec<String> = vec![hours, minutes, seconds]
+                .into_iter()
+                .map(|number| format!("{number}"))
+                .map(|time_str| match time_str.chars().count() {
+                    1 => format!("0{time_str}"),
+                    _ => time_str,
+                })
+                .collect();
+
+            format!("{}:{}:{}", time[0], time[1], time[2])
+        }
+
         PodInfo {
-            guid: default_guid(),
-            // episode: todo!(),
+            guid: GuidBuilder::default()
+                .value(Uuid::new_v4().as_simple().to_string())
+                .permalink(false)
+                .build(),
+            episode: None,
             title: video.title,
-            duration_str: duration.format("%H:%M:%S").to_string(),
+            duration_str: gen_duration_str(duration),
             duration_secs: duration.num_seconds(),
-            author: todo!(),
-            date: todo!(),
-            link: todo!(),
-            description: todo!(),
+            author: video
+                .uploader
+                .expect(&format!("Could not find an uploader for {}", video.id)),
+            date: format!("{}", date.format("%a, %d %b %Y 00:00:00 +0000")), // ex: Thu, 06 Oct 2022 15:07:56 +0000
+            link: video
+                .webpage_url
+                .expect(&format!("Could not find a url for {}", video.id)),
+            description: gen_description(video.description),
         }
     }
-}
-
-fn default_guid() -> Guid {
-    GuidBuilder::default()
-        .value(Uuid::new_v4().as_simple().to_string())
-        .permalink(false)
-        .build()
 }
 
 fn build_episode(video: SingleVideo) -> Item {
@@ -141,7 +188,9 @@ pub async fn gen_feed(channel_id: String) {
 
     let latest = recents.clone().into_iter().nth(0).unwrap();
     let wow = latest.clone();
-    println!("{wow:#?}");
+    // println!("wow: {wow:#?}");
+    let wow2: PodInfo = latest.clone().into();
+    println!("wow2: {wow2:#?}");
 
     let ep = build_episode(latest);
     let itunes_metadata = ITunesChannelExtensionBuilder::default()
