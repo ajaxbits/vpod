10b47074f56ed07e4678ef245c968a906c7c8c03
Alex Jackson <contact@ajaxbits.com>
2022-12-31 12:33:40 -0600

    feat: add support for @handles

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use axum::{
     Router,
 };
 use core::panic;
-use episode::Episode;
 use feed::Feed;
 use rss::Channel;
+use serde::Deserialize;
 use std::{
     fs::File,
     io::{self, BufReader},
@@ -25,9 +25,11 @@ mod gen_feed;
 mod polling;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
     let app = Router::new()
-        .route("/:cpfx/:channel_name", get(serve_rss))
+        .route("/:path_type/*val", get(serve_rss))
         .route("/ep/:id", get(return_audio));
 
     let addr = SocketAddr::from_str("[::]:8080").expect("could not parse socketaddr");
@@ -35,40 +37,68 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .expect("could not start server");
+
+    Ok(())
 }
 
-async fn return_audio(Path(id): Path<String>) -> impl IntoResponse {
-    let url = format!("https://www.youtube.com/watch?v={id}");
-    let path = format!("{id}.m4a");
-    if let false = std::path::Path::new(&path).exists() {
-        let args = vec![
-            Arg::new("--quiet"),
-            Arg::new_with_arg("--format", "bestaudio[protocol^=http][abr<100][ext=m4a]"),
-            Arg::new("--embed-metadata"),
-            Arg::new("--embed-thumbnail"),
-            Arg::new_with_arg("--sponsorblock-mark", "sponsor,selfpromo"),
-            Arg::new_with_arg("--output", "%(id)s.m4a"),
-        ];
-        let _ytd = ytd_rs::YoutubeDL::new(&PathBuf::from("./."), args, &url)
-            .unwrap()
-            .download();
+#[derive(Deserialize)]
+struct YtPath {
+    path_type: YtPathType,
+    val: Option<String>,
+}
+
+#[derive(Debug)]
+enum YtPathType {
+    Handle(String),
+    Abbrev(String),
+    Full(String),
+    User(String),
+}
+
+impl<'de> Deserialize<'de> for YtPathType {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        if &s[..1] == "@" {
+            Ok(Self::Handle(s))
+        } else if &s == "c" {
+            Ok(Self::Abbrev(s))
+        } else if &s == "channel" {
+            Ok(Self::Full(s))
+        } else if &s == "user" {
+            Ok(Self::User(s))
+        } else {
+            Err(serde::de::Error::custom(format!(
+                "Invalid path type '{}'",
+                &s
+            )))
+        }
     }
-
-    let req = Request::builder()
-        .uri(id)
-        .body(axum::body::Body::empty())
-        .unwrap();
-
-    let service =
-        get_service(tower_http::services::ServeFile::new(path)).handle_error(handle_error);
-
-    let result = service.oneshot(req).await;
-
-    result
 }
 
-async fn serve_rss(Path((cpfx, id)): Path<(String, String)>) -> impl IntoResponse {
-    let yt_url = format!("https://www.youtube.com/{cpfx}/{id}");
+// #[axum::debug_handler]
+async fn serve_rss(Path(YtPath { path_type, val }): Path<YtPath>) -> impl IntoResponse {
+    let yt_url = match dbg!(path_type) {
+        YtPathType::Handle(handle) => format!("https://www.youtube.com/{handle}"),
+        YtPathType::Abbrev(type_string) => format!(
+            "https://www.youtube.com/{}/{}",
+            type_string,
+            val.expect("The Abbrev path type must have a val")
+        ),
+        YtPathType::Full(type_string) => format!(
+            "https://www.youtube.com/{}/{}",
+            type_string,
+            val.expect("The Full path type must have a val")
+        ),
+        YtPathType::User(type_string) => format!(
+            "https://www.youtube.com/{}/{}",
+            type_string,
+            val.expect("The Full path type must have a val")
+        ),
+    };
+
     let id = vpod::get_channel_id(&yt_url)
         .await
         .expect("could not get channel_id");
@@ -98,6 +128,37 @@ async fn serve_rss(Path((cpfx, id)): Path<(String, String)>) -> impl IntoRespons
     let channel = rss::Channel::from(feed.clone());
     let file = File::create(&path).unwrap_or_else(|_| panic!("could ot create {id}.xml"));
     channel.write_to(file).unwrap();
+
+    let result = service.oneshot(req).await;
+
+    result
+}
+
+// #[axum::debug_handler]
+async fn return_audio(Path(id): Path<String>) -> impl IntoResponse {
+    let url = format!("https://www.youtube.com/watch?v={id}");
+    let path = format!("{id}.m4a");
+    if let false = std::path::Path::new(&path).exists() {
+        let args = vec![
+            Arg::new("--quiet"),
+            Arg::new_with_arg("--format", "bestaudio[protocol^=http][abr<100][ext=m4a]"),
+            Arg::new("--embed-metadata"),
+            Arg::new("--embed-thumbnail"),
+            Arg::new_with_arg("--sponsorblock-mark", "sponsor,selfpromo"),
+            Arg::new_with_arg("--output", "%(id)s.m4a"),
+        ];
+        let _ytd = ytd_rs::YoutubeDL::new(&PathBuf::from("./."), args, &url)
+            .unwrap()
+            .download();
+    }
+
+    let req = Request::builder()
+        .uri(id)
+        .body(axum::body::Body::empty())
+        .unwrap();
+
+    let service =
+        get_service(tower_http::services::ServeFile::new(path)).handle_error(handle_error);
 
     let result = service.oneshot(req).await;
 
